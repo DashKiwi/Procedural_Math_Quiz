114d9c25bdbfbb7d99061995a08da432b457fb28
DashKiwi <Maxmooney08@gmail.com>
2024-03-19 22:54:52 +1300

    Added all functionality
    Everything that is wanted in the code is done just need to make it look better. might add question history??

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ question_types = ["+", "/", "-", "x"]
 
 correct_answers = 0
 question_count = 0
+count = 0
+question_amount = 0
+
+def float_checker(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except:
+            print("Please enter a valid Number")
 
 
-def one_question():
+def one_question(number):
     global question_types, correct_answers, question_count
     correct_answer = 0
-    print("Question ")
+    print("Question {}".format(number))
     # Chooses 2 random numbers
     number_one = random.randrange(1, 20)
     number_two = random.randrange(1, 20)
@@ -24,9 +33,10 @@ def one_question():
         correct_answer = number_one - number_two
     elif question_choice == "x":
         correct_answer = number_one * number_two
-
-    user_answer = float_checker(input("What is {} {} {}\n".format(number_one, question_choice, number_two)))
-    question_count += 1
+    
+    user_answer = float_checker("What is {} {} {} rounded to the first decimal place\n".format(number_one, question_choice, number_two))
+    user_answer = round(user_answer, 1)
+    correct_answer = round(correct_answer, 1)
 
     if correct_answer == user_answer:
         print("Yay you got it right!")
@@ -34,10 +44,26 @@ def one_question():
     else:
         print("Oh no you got it wrong. The correct answer was {}!".format(correct_answer))
 
+print(".___  ___.      ___   .___________. __    __       ______      __    __   __   ________  \n" +
+"|   \/   |     /   \  |           ||  |  |  |     /  __  \    |  |  |  | |  | |       /  \n" +
+"|  \  /  |    /  ^  \ `---|  |----`|  |__|  |    |  |  |  |   |  |  |  | |  | `---/  /   \n" +
+"|  |\/|  |   /  /_\  \    |  |     |   __   |    |  |  |  |   |  |  |  | |  |    /  /    \n" +
+"|  |  |  |  /  _____  \   |  |     |  |  |  |    |  `--'  '--.|  `--'  | |  |   /  /----.\n" +
+"|__|  |__| /__/     \__\  |__|     |__|  |__|    \_____\______\\________/ |__|  /________|\n" +
+"~~~~ BY MAXIAN MOONEY ~~~~\n")
 
-question_amount = input("How many Questions do you want?\n")
 
-for i in question_amount:
-    one_question()
-
-print("Thank you for playing your total score was {}/{}".format(0, question_count))
+while True:
+    user_input = int(input("How many rounds do you want to play?\n"))
+    round_number = 0
+    for i in range(user_input):
+        count += 1
+        one_question(count)
+    user_input = input("Would you like another question? (Y/N)\n").upper()
+    if user_input == "YES" or user_input == "Y":         
+        pass
+    elif user_input == "NO" or user_input == "N":
+        print("~~~~~~ SUMMARY ~~~~~~")
+        print("Your Score was {}/{} questions correct!".format(correct_answers, count))
+        print("Thank you for playing come back soon!")
+        break
