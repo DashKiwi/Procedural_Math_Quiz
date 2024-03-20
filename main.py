2f0010b2a7aaf3c2dff765b83acd29c6bc7c1e75
DashKiwi <Maxmooney08@gmail.com>
2024-03-20 20:48:44 +1300

    Made little changes
    made the code look a bit better. started implementing history

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def float_checker(prompt):
 def one_question(number):
     global question_types, correct_answers, previous_questions, previous_answers, previous_user_answers
     correct_answer = 0
-    print("Question {}".format(number))
+    print("Question {}\n".format(number))
     # Chooses 2 random numbers
     number_one = random.randrange(1, 20)
     number_two = random.randrange(1, 20)
@@ -36,23 +36,22 @@ def one_question(number):
         correct_answer = number_one * number_two
     
     #asks for the users anser and makes it a float
-    user_answer = float_checker("What is {} {} {} rounded to the first decimal place\n".format(number_one, question_choice, number_two))
+    prompt = ("What is {} {} {} rounded to the first decimal place\n".format(number_one, question_choice, number_two))
+    user_answer = float_checker(prompt)
     #rounds user and correct answer to the first decimal place
     user_answer = round(user_answer, 1)
     correct_answer = round(correct_answer, 1)
 
     #checks if the answer is correct
     if correct_answer == user_answer:
-        print("Yay you got it right!")
+        print("Yay you got it right!\n")
         correct_answers += 1
     else:
         print("Oh no you got it wrong. The correct answer was {}!".format(correct_answer))
     #adds the history to the lists 
     previous_user_answers.append(user_answer)
     previous_answers.append(correct_answer)
-    previous_questions.append(number_one)
-    previous_questions.append(question_choice)
-    previous_questions.append(number_two)
+    previous_questions.append(prompt)
 
 #Start menu
 print(".___  ___.      ___   .___________. __    __       ______      __    __   __   ________  \n" +
@@ -68,25 +67,24 @@ while True:
     #resets the question types when rerun
     question_types = ["+", "/", "-", "x"]
     #asks what type of operator they want
-    user_input = input("Please choose one of the following operators or type any for all of them (+, X, /, -)\n").upper()
+    operator_input = input("Please choose one of the following operators or type all for all of them (+, X, /, -)\n").upper()
     #checks the users answer
-    if user_input == "ALL":
+    if operator_input == "ALL":
         pass
-    elif user_input == "+":
+    elif operator_input == "+":
         question_types = ["+"]
-    elif user_input == "X":
+    elif operator_input == "X":
         question_types = ["x"]
-    elif user_input == "/":
+    elif operator_input == "/":
         question_types = ["/"]
-    elif user_input == "-":
+    elif operator_input == "-":
         question_types = ["-"]
     else:
         print("Please choose +, X, /, - or all to begin")
     #asks how many rounds they want to play
-    user_input = int(input("How many rounds do you want to play?\n"))
-    round_number = 0
+    round_input = int(input("How many rounds do you want to play?\n"))
     #for every round they want to play add 1 to the count and run one_question with the count paramater
-    for i in range(user_input):
+    for i in range(round_input):
         count += 1
         one_question(count)
     #asks after the code has finished if they want another question
